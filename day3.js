34742087ba53dfd679aa1a856635438f2b334a3c
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-01-25 11:17:04 +0800

    Update day3.js

--- day3.js
+++ day3.js
@@ -8,3 +8,12 @@ function evenOrOdd(number) {
         return "odd";
     }
 }
+
+//Complete the function getNumberOfElements such that it returns the number of items contained in the elements array it receives.
+function getNumberOfElements(elements) {
+    return elements.length;
+}
+
+// Sample usage - do not modify
+console.log(getNumberOfElements(['a', 'b', 'c'])); // 3
+console.log(getNumberOfElements([])); // 0
