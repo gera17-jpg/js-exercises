8f392e98b6f5865fbcd04bdf7a89af41f5db0d70
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-01-25 11:20:16 +0800

    Update day3.js

--- day3.js
+++ day3.js
@@ -17,3 +17,10 @@ function getNumberOfElements(elements) {
 // Sample usage - do not modify
 console.log(getNumberOfElements(['a', 'b', 'c'])); // 3
 console.log(getNumberOfElements([])); // 0
+
+/*Complete the function useCalculator such that it adds the "Calculator" string to the apps array it receives. Then, return the apps array.
+There is a small trick in this challenge to make sure you understand an important concept. Spend some time trying to figure it out, then check the hints if you get stuck.*/
+function useCalculator(apps) {
+    apps.push("Calculator");
+    return apps;
+}
